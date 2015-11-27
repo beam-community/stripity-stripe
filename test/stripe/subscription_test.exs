ee638659c66529fbcf98da3e60cf55f5e4286f64
nicrioux <nic@biospheric.co>
2015-11-26 23:23:32 -0800

    refactored test setup/teardown for better readability

--- test/stripe/subscription_test.exs
+++ test/stripe/subscription_test.exs
@@ -2,50 +2,12 @@ defmodule Stripe.SubscriptionTest do
   use ExUnit.Case
 
   setup do
-    #create test plan
-    case Stripe.Plans.create([id: "standard", name: "standard", amount: 1000]) do
-        {:ok, plan} -> assert plan.id == "standard"
-        {:error, err} -> flunk err
-    end
-    case Stripe.Plans.create([id: "premium", name: "premium", amount: 2000]) do
-      {:ok, plan} -> assert plan.id == "premium"
-      {:error, err} -> flunk err
-    end
-    #cleanup test plan
-    on_exit fn ->
-      case Stripe.Plans.delete "standard" do
-        {:ok, plan} -> assert plan.deleted
-        {:error, err} -> flunk err
-      end
-      case Stripe.Plans.delete "premium" do
-        {:ok, plan} -> assert plan.deleted
-        {:error, err} -> flunk err
-      end
-    end
-
-        #create a sub with nothing but a card
-    new_sub = [
-      email: "jill@test.com",
-      description: "Jill Test Account",
-      plan: "standard",
-      source: [
-        object: "card",
-        number: "4111111111111111",
-        exp_month: 01,
-        exp_year: 2018,
-        cvc: 123,
-        name: "Jill Subscriber"
-      ]
-    ]
-
-    case Stripe.Customers.create_subscription new_sub do
-      {:ok, customer} -> {:ok, [customer: customer, sub: List.first(customer.subscriptions["data"])]}
-      {:error, err} -> flunk err
-    end
-
+    setup_plans
+    register_teardown_plans
+    setup_subscription
   end
 
-@tag disabled: false
+    @tag disabled: false
   test "Listing subscriptions works", %{customer: customer, sub: sub} do
     case Stripe.Customers.get_subscriptions customer.id do
       {:ok, subs} -> assert subs
@@ -82,4 +44,51 @@ defmodule Stripe.SubscriptionTest do
     end
   end
 
+  #create test plans: standard and premium
+  defp setup_plans do
+    case Stripe.Plans.create([id: "standard", name: "standard", amount: 1000]) do
+      {:ok, plan} -> assert plan.id == "standard"
+      {:error, err} -> flunk err
+    end
+    case Stripe.Plans.create([id: "premium", name: "premium", amount: 2000]) do
+      {:ok, plan} -> assert plan.id == "premium"
+      {:error, err} -> flunk err
+    end
+  end
+
+  #cleanup test plans
+  defp register_teardown_plans do
+    on_exit fn ->
+        case Stripe.Plans.delete "standard" do
+            {:ok, plan} -> assert plan.deleted
+            {:error, err} -> flunk err
+        end
+        case Stripe.Plans.delete "premium" do
+            {:ok, plan} -> assert plan.deleted
+            {:error, err} -> flunk err
+        end
+    end
+   end
+
+  #create a sub with nothing but a card
+  defp setup_subscription do
+    new_sub = [
+      email: "jill@test.com",
+      description: "Jill Test Account",
+      plan: "standard",
+      source: [
+        object: "card",
+        number: "4111111111111111",
+        exp_month: 01,
+        exp_year: 2018,
+        cvc: 123,
+        name: "Jill Subscriber"
+      ]
+    ]
+
+    case Stripe.Customers.create_subscription new_sub do
+      {:ok, customer} -> {:ok, [customer: customer, sub: List.first(customer.subscriptions["data"])]}
+      {:error, err} -> flunk err
+    end
+  end
 end
