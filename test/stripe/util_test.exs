046d2063f5091723644fd7e707f23d1694748091
Eugene Pirogov <iamexile@gmail.com>
2016-07-20 16:15:16 +0300

    Wrap tests in test/stripe/util_test.exs

--- test/stripe/util_test.exs
+++ test/stripe/util_test.exs
@@ -1,55 +1,72 @@
 defmodule Stripe.UtilTest do
   use ExUnit.Case
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
 
   setup_all do
-    Stripe.Plans.delete_all
-    Helper.create_test_plans
-
-    on_exit fn ->
+    use_cassette "Stripe.UtilTest/setup", match_requests_on: [:query, :request_body] do
       Stripe.Plans.delete_all
+      Helper.create_test_plans
+
+      on_exit fn ->
+        use_cassette "Stripe.UtilTest/teardown", match_requests_on: [:query, :request_body] do
+          Stripe.Plans.delete_all
+        end
+      end
+      :ok
     end
-    :ok
   end
 
   test "Count" do
-    case Stripe.Util.count "plans" do
-      {:ok, cnt} -> assert cnt == 2
-      {:error, err} -> flunk err
+    use_cassette "Stripe.UtilTest/count", match_requests_on: [:query, :request_body] do
+      case Stripe.Util.count "plans" do
+        {:ok, cnt} -> assert cnt == 2
+        {:error, err} -> flunk err
+      end
     end
   end
   test "Count w/key" do
-    case Stripe.Util.count "plans", Stripe.config_or_env_key do
-      {:ok, cnt} -> assert cnt == 2
-      {:error, err} -> flunk err
+    use_cassette "Stripe.UtilTest/count_with_key", match_requests_on: [:query, :request_body] do
+      case Stripe.Util.count "plans", Stripe.config_or_env_key do
+        {:ok, cnt} -> assert cnt == 2
+        {:error, err} -> flunk err
+      end
     end
   end
   test "list_raw" do
-    case Stripe.Util.list_raw "plans" do
-      {:ok, plans} -> assert plans
-      {:error, err} -> flunk err
+    use_cassette "Stripe.UtilTest/list_raw", match_requests_on: [:query, :request_body] do
+      case Stripe.Util.list_raw "plans" do
+        {:ok, plans} -> assert plans
+        {:error, err} -> flunk err
+      end
     end
   end
   test "list_raw w/key" do
-    case Stripe.Util.list_raw "plans", Stripe.config_or_env_key, 10, "" do
-      {:ok, plans} -> assert plans
-      {:error, err} -> flunk err
+    use_cassette "Stripe.UtilTest/list_raw_with_key", match_requests_on: [:query, :request_body] do
+      case Stripe.Util.list_raw "plans", Stripe.config_or_env_key, 10, "" do
+        {:ok, plans} -> assert plans
+        {:error, err} -> flunk err
+      end
     end
   end
 
   @tag disabled: false
   test "list works" do
-    case Stripe.Util.list "plans" do
-      {:ok, resp} ->
-        assert Dict.size(resp[:data]) == 2
-      {:error, err} -> flunk err
+    use_cassette "Stripe.UtilTest/list", match_requests_on: [:query, :request_body] do
+      case Stripe.Util.list "plans" do
+        {:ok, resp} ->
+          assert Dict.size(resp[:data]) == 2
+          {:error, err} -> flunk err
+      end
     end
   end
 
   @tag disabled: false
   test "list w/key works" do
-    case Stripe.Util.list "plans", Stripe.config_or_env_key,  "", 2  do
-      {:ok, resp} -> assert Dict.size( resp[:data]) == 2
-      {:error, err} -> flunk err
+    use_cassette "Stripe.UtilTest/list_with_key", match_requests_on: [:query, :request_body] do
+      case Stripe.Util.list "plans", Stripe.config_or_env_key,  "", 2  do
+        {:ok, resp} -> assert Dict.size( resp[:data]) == 2
+        {:error, err} -> flunk err
+      end
     end
   end
 end
