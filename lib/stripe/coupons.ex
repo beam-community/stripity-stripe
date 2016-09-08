446fdca9f058da2727b07cbea8895d7d0c25227c
beerlington <pete@lette.us>
2016-09-08 12:28:17 -0400

    Adds support for creating coupons

--- lib/stripe/coupons.ex
+++ lib/stripe/coupons.ex
@@ -4,7 +4,7 @@ defmodule Stripe.Coupons do
   (API ref: https://stripe.com/docs/api#coupons)
 
   Operations:
-  - create (TODO)
+  - create
   - retrieve
   - update (TODO)
   - delete (TODO)
@@ -29,6 +29,49 @@ defmodule Stripe.Coupons do
     path = @endpoint <> "/" <> params
 
     Stripe.make_request(:get, path, %{}, %{})
+    |> Stripe.Util.handle_stripe_response
+  end
+
+  @doc """
+  Creates a coupon with given options. :duration is required and must be
+  either "forever", "once", or "repeating".
+  Returns a coupon if successful.
+  Throws an error otherwise.
+
+  ## Examples
+  ```
+    opts = [
+      duration: "forever",
+      id: "DISCOUNT20",
+      percent_off: 25
+    ]
+
+    {:ok, result} = Stripe.Coupons.create opts
+  ```
+  """
+  def create(opts \\ []) do
+    create opts, Stripe.config_or_env_key
+  end
+
+  @doc """
+  Creates a coupon with given options using given API key. :duration is required and must be
+  either "forever", "once", or "repeating".
+  Returns a coupon if successful.
+  Throws an error otherwise.
+
+  ## Examples
+  ```
+    opts = [
+      duration: "forever",
+      id: "DISCOUNT20",
+      percent_off: 25
+    ]
+
+    {:ok, result} = Stripe.Coupons.create opts, key
+  ```
+  """
+  def create(opts, key) do
+    Stripe.make_request_with_key(:post, @endpoint, key, opts)
     |> Stripe.Util.handle_stripe_response
   end
 end
