3c5db42891f3a196a061cfd78756c392e6c2878a
David Antaramian <david@antaramian.com>
2016-11-04 04:11:00 -0400

    Rearranged to use :hackney

--- lib/stripe.ex
+++ lib/stripe.ex
@@ -1,149 +1,266 @@
 defmodule Stripe do
   @moduledoc """
-    A HTTP client for Stripe.
-    This module contains the Application that you can use to perform
-    transactions on stripe API.
-    ### Configuring
-    By default the STRIPE_SECRET_KEY environment variable is used to find
-    your API key for Stripe. You can also manually set your API key by
-    configuring the :stripity_stripe application. You can see the default
-    configuration in the default_config/0 private function at the bottom of
-    this file. The value for platform client id is optional.
+  A HTTP client for Stripe.
 
-      config :stripity_stripe, secret_key: YOUR_STRIPE_KEY
-      config :stripity_stripe, platform_client_id: STRIPE_PLATFORM_CLIENT_ID
+  ## Configuration
+
+  ### API Key
+  
+  You need to set your API key in your application configuration. Typically
+  this is done in `config/config.exs` or a similar file. For example:
+
+      config :stripity_stripe, api_key: "sk_test_abc123456789qwerty"
+
+  You can also utilize `System.get_env/1` to retrieve the API key from
+  an environment variable, but remember that this can cause issues if
+  you use a release tool like exrm or Distillery.
+
+      config :stripity_stripe, api_key: System.get_env("STRIPE_API_KEY")
+
+  ### HTTP Connection Pool
+  
+  Stripity Stripe is set up to use an HTTP connection pool by default. This
+  means that it will reuse already opened HTTP connections in order to
+  minimize the overhead of establishing connections. Two configuration
+  options are available to tune how this pool works: `:timeout` and
+  `:max_connections`.
+
+  `:timeout` is the amount of time that a connection will be allowed
+  to remain open but idle (no data passing over it) before it is closed
+  and cleaned up. This defaults to 5 seconds.
+
+  `:max_connections` is the maximum number of connections that can be
+  open at any time. This defaults to 10.
+
+  Both these settings are located under the `:pool_options` key in
+  your application configuration:
+
+      config :stripity_stripe, :pool_options,
+        timeout: 5_000,
+        max_connections: 10
+
+  If you prefer, you can also turn pooling off completely using
+  the `:use_connection_pool` setting:
+
+      config :stripity_stripe, use_connection_pool: false
+
   """
 
-  # Let's build on top of HTTPoison
-  use HTTPoison.Base
-
-  defmodule MissingSecretKeyError do
+  defmodule MissingAPIKeyError do
     defexception message: """
-      The secret_key setting is required so that we can report the
-      correct environment instance to Stripe. Please configure
-      secret_key in your config.exs and environment specific config files
-      to have accurate reporting of errors.
-      config :stripity_stripe, secret_key: YOUR_SECRET_KEY
+    The secret_key setting is required so that we can report the
+    correct environment instance to Stripe. Please configure
+    secret_key in your config.exs and environment specific config files
+    to have accurate reporting of errors.
+    config :stripity_stripe, api_key: YOUR_SECRET_KEY
     """
   end
 
-
-  @doc """
-  Grabs STRIPE_SECRET_KEY from system ENV
-  Returns binary
-  """
-  def config_or_env_key do
-    require_stripe_key()
+  defmodule APIAuthenticationError do
+    defexception message: """
+    Stripe could not authenticate the request with the provided API key
+    """
   end
 
-  @doc """
-  Grabs STRIPE_PLATFORM_CLIENT_ID from system ENV
-  Returns binary
-  """
-  def config_or_env_platform_client_id do
-    Application.get_env(:stripity_stripe, :platform_client_id) || System.get_env "STRIPE_PLATFORM_CLIENT_ID"
+  defmodule APIRateLimitingError do
+    defexception message: """
+    Stripe is currently limiting the rate at which it accepts your HTTP requests
+    """
   end
 
-  @doc """
-  Creates the URL for our endpoint.
-  Args:
-    * endpoint - part of the API we're hitting
-  Returns string
-  """
-  def process_url(endpoint) do
-    "https://api.stripe.com/v1/" <> endpoint
-  end
+  defmodule APIError do
+    defexception [:message, :type, :status_code, :code]
 
-  @doc """
-  Set our request headers for every request.
-  """
-  def req_headers(key) do
-    Map.new
-      |> Map.put("Authorization", "Bearer #{key}")
-      |> Map.put("User-Agent",    "Stripe/v1 stripity-stripe/1.4.0")
-      |> Map.put("Content-Type",  "application/x-www-form-urlencoded")
-  end
+    @type t :: %__MODULE__{
+      message: String.t | nil,
+      type: String.t | nil,
+      status_code: pos_integer | nil,
+      code: String.t | nil
+    }
 
-  @doc """
-  Converts the binary keys in our response to atoms.
-  Args:
-    * body - string binary response
-  Returns Record or ArgumentError
-  """
-  def process_response_body(body) do
-    Poison.decode! body
-  end
+    @spec exception({integer, map}) :: t
+    def exception({status_code, %{"type" => type, "message" => message} = body}) do
+      # code is not a guaranteed key
+      code = Map.get(body, "code")
 
-  @doc """
-  Boilerplate code to make requests with a given key.
-  Args:
-    * method - request method
-    * endpoint - string requested API endpoint
-    * key - stripe key passed to the api
-    * body - request body
-    * headers - request headers
-    * options - request options
-  Returns tuple
-  """
-  def make_request_with_key( method, endpoint, key, body \\ %{}, headers \\ %{}, options \\ []) do
-    rb = Stripe.URI.encode_query(body)
-    rh = req_headers(key)
-        |> Map.merge(headers)
-        |> Map.to_list
+      %__MODULE__{
+        message: message,
+        type: type,
+        status_code: status_code,
+        code: code
+      }
+    end
 
-    options = Keyword.merge(httpoison_request_options(), options)
-    {:ok, response} = request(method, endpoint, rb, rh, options)
-    response.body
-  end
+    def exception({status_code, _}) do
+      msg = """
+      The Stripe HTTP client received an error response with status code #{status_code}
+      """
 
-  @doc """
-  Boilerplate code to make requests with the key read from config or env.see config_or_env_key/0
-  Args:
-  * method - request method
-  * endpoint - string requested API endpoint
-  * key - stripe key passed to the api
-  * body - request body
-  * headers - request headers
-  * options - request options
-  Returns tuple
-  """
-  def make_request(method, endpoint, body \\ %{}, headers \\ %{}, options \\ []) do
-    make_request_with_key(method, endpoint, config_or_env_key(), body, headers, options)
-  end
-
-  @doc """
-  """
-  def make_oauth_token_callback_request(body) do
-    rb = Stripe.URI.encode_query(body)
-    rh = req_headers(Stripe.config_or_env_key)
-      |> Map.to_list
-
-    options = httpoison_request_options()
-    HTTPoison.request(:post, "#{Stripe.Connect.base_url}oauth/token", rb, rh, options)
-  end
-
-  @doc """
-  """
-  def make_oauth_deauthorize_request(stripe_user_id) do
-    rb = Stripe.URI.encode_query([
-      stripe_user_id: stripe_user_id,
-      client_id: Stripe.config_or_env_platform_client_id])
-    rh = req_headers(Stripe.config_or_env_key)
-      |> Map.to_list
-
-    options = httpoison_request_options()
-    HTTPoison.request(:post, "#{Stripe.Connect.base_url}oauth/deauthorize", rb, rh, options)
-  end
-
-  defp require_stripe_key do
-    case Application.get_env(:stripity_stripe, :secret_key, System.get_env "STRIPE_SECRET_KEY") || :not_found do
-      :not_found ->
-        raise MissingSecretKeyError
-      value -> value
+      %__MODULE__{
+        message: msg,
+        status_code: status_code
+      }
     end
   end
 
-  defp httpoison_request_options() do
-    Application.get_env(:stripity_stripe, :httpoison_options, [])
+  defmodule HTTPError do
+    defexception message: """
+    The Stripe HTTP client encountered an error while communicating with the
+    Stripe service.
+    """
+  end
+
+  alias __MODULE__.{MissingAPIKeyError, HTTPError, APIRateLimitingError, APIError}
+
+  @type method :: :get | :post | :put | :delete | :patch
+  @type headers :: %{required(String.t) => String.t}
+  @typep http_success :: {:ok, integer, [{String.t, String.t}], String.t}
+  @typep http_failure :: {:error, term}
+
+  @pool_name __MODULE__
+
+  @doc """
+  Starts necessary components for the HTTP client to function
+  """
+  @spec start() :: :ok
+  def start() do
+    if use_pool?() do
+      pool_options = get_pool_options()
+      :ok = :hackney_pool.start_pool(@pool_name, pool_options)
+    end
+
+    :ok
+  end
+
+  @doc """
+  Cleans up the HTTP client's components (typically unnecessary to call)
+  """
+  @spec stop() :: :ok
+  def stop() do
+    :ok = :hackney_pool.stop_pool(@pool_name)
+  end
+
+  @spec get_pool_options() :: Keyword.t
+  defp get_pool_options() do
+    Application.get_env(:stripity_stripe, :pool_options)
+  end
+
+  @spec get_base_url() :: String.t
+  defp get_base_url() do
+    Application.get_env(:stripity_stripe, :api_base_url)
+  end
+
+  @spec get_api_key() :: String.t
+  defp get_api_key() do
+    case Application.get_env(:stripity_stripe, :api_key) do
+      nil -> raise MissingAPIKeyError
+      key -> key
+    end
+  end
+
+  @spec use_pool?() :: boolean
+  defp use_pool?() do
+    Application.get_env(:stripity_stripe, :use_connection_pool)
+  end
+
+  @spec add_default_headers(headers) :: headers
+  defp add_default_headers(existing_headers) do
+    api_key = get_api_key()
+    version_num = Application.spec(:stripity_stripe, :vsn)
+
+    Map.merge(existing_headers, %{
+      "Accept" => "application/json; charset=utf8",
+      "Accept-Encoding" => "gzip",
+      "Authorization" => "Bearer #{api_key}",
+      "Content-Type" => "applicaton/x-www-form-urlencoded",
+      "Connection" => "keep-alive",
+      "User-Agent" => "Stripe/v1 stripity-stripe/#{version_num}"
+    })
+  end
+
+  @spec add_connect_header(headers, String.t | nil) :: headers
+  defp add_connect_header(existing_headers, nil), do: existing_headers
+  defp add_connect_header(existing_headers, account_id) do
+    Map.put(existing_headers, "Stripe-Account", account_id)
+  end
+
+  @spec add_default_options(Keyword.t) :: Keyword.t
+  defp add_default_options(opts) do
+    Keyword.merge(opts, [with_body: opts])
+  end
+
+  @spec add_pool_option(Keyword.t) :: Keyword.t
+  defp add_pool_option(opts) do
+    if use_pool?() do
+      Keyword.put(opts, :pool, @pool_name)
+    else
+      opts
+    end
+  end
+
+  @doc """
+  A low level utility function to make a direct request to the Stripe API
+
+  ## Connect Accounts
+
+  If you'd like to make a request on behalf of another Stripe account
+  utilizing the Connect program, you can pass the other Stripe account's
+  ID to the request function as follows:
+
+      request(:get, "/customers", %{}, %{}, connect_account: "acc_134151")
+
+  """
+  @spec request(method, String.t, map, headers, Keyword.t) :: {:ok, map} | {:error, Exception.t}
+  def request(method, endpoint, body, headers, opts) do
+    {connect_account_id, opts} = Keyword.pop(opts, :connection_account)
+
+    req_url = get_base_url() <> endpoint
+    req_body = Stripe.URI.encode_query(body)
+    req_headers =
+      headers
+      |> add_default_headers()
+      |> add_connect_header(connect_account_id)
+      |> Map.to_list()
+
+    req_opts =
+      opts
+      |> add_default_options()
+      |> add_pool_option()
+
+    :hackney.request(method, req_url, req_headers, req_body, req_opts)
+    |> handle_response()
+  end
+
+  @spec handle_response(http_success | http_failure) :: {:ok, map} | {:error, Exception.t}
+  defp handle_response({:ok, status, _headers, body}) when status in 200..299 do
+    decoded_body = Poison.decode!(body)
+
+    {:ok, decoded_body}
+  end
+
+  defp handle_response({:ok, 401, _headers, body}) do
+    %{"error" => api_error} = Poison.decode!(body)
+    error = APIAuthenticationError.exception({401, api_error})
+
+    {:error, error}
+  end
+
+  defp handle_response({:ok, 429, _headers, body}) do
+    %{"error" => api_error} = Poison.decode!(body)
+    error = APIRateLimitingError.exception({429, api_error})
+
+    {:error, error}
+  end
+
+  defp handle_response({:ok, status, _headers, body}) when status in 400..599 do
+    %{"error" => api_error} = Poison.decode!(body)
+    error = APIError.exception({status, api_error})
+
+    {:error, error}
+  end
+
+  defp handle_response({:error, _reason}) do
+    error = HTTPError.exception(nil)
+    {:error, error}
   end
 end
--- mix.exs
+++ mix.exs
@@ -20,19 +20,39 @@ defmodule Stripe.Mixfile do
   # Configuration for the OTP application
   def application do
     [
-      applications: [:httpoison]
+      env: env(),
+      applications: apps(Mix.env)
     ]
   end
 
+  defp env() do
+    [
+      stripity_stripe: [
+        api_version: "2016-07-06",
+        api_base_url: "https://api.stripe.com/v1/",
+        use_connection_pool: true,
+        pool_options: [
+          timeout: 5_000,
+          max_connections: 10
+        ]
+      ]
+    ]
+  end
+
+  defp apps(:test), do: [:bypass | apps()]
+  defp apps(_), do: apps()
+
+  defp apps(), do: [:hackney, :poison, :logger]
+
   defp deps do
     [
-      {:httpoison, ">= 0.0.0" },
-      {:poison, ">= 0.0.0", optional: true},
-      {:ex_doc, ">= 0.0.0", only: :dev},
-      {:earmark, ">= 0.0.0", only: :dev},
-      {:excoveralls, ">= 0.0.0", only: :test},
-      {:mock, ">= 0.0.0", only: :test},
-      {:inch_ex, ">= 0.0.0", only: [:dev, :test]}
+      {:hackney, "~> 1.6"},
+      {:poison, "~> 2.0"},
+      {:bypass, "~> 0.5", only: :test},
+      {:ex_doc, "~> 0.14", only: :dev},
+      {:earmark, "~> 1.0", only: :dev},
+      {:excoveralls, "~> 0.5", only: :test},
+      {:inch_ex, "~> 0.5", only: [:dev, :test]}
     ]
   end
 
@@ -48,8 +68,7 @@ defmodule Stripe.Mixfile do
       maintainers: ["Dan Matthews", "Josh Smith"],
       licenses: ["New BSD"],
       links: %{
-        "GitHub" => "https://github.com/code-corps/stripity-stripe",
-        "Docs" => "https://hexdocs.pm/stripity_stripe"
+        "GitHub" => "https://github.com/code-corps/stripity-stripe"
       }
     ]
   end
