f2ccdf8e80ee95a8570c736a2e0448ebd8e60850
David Antaramian <david@antaramian.com>
2016-11-04 04:11:00 -0400

    Implement low-level API

--- mix.exs
+++ mix.exs
@@ -2,7 +2,8 @@ defmodule Stripe.Mixfile do
   use Mix.Project
 
   def project do
-    [ app: :stripity_stripe,
+    [
+      app: :stripity_stripe,
       version: "2.0.0",
       description: description(),
       package: package(),
@@ -14,25 +15,46 @@ defmodule Stripe.Mixfile do
         "coveralls.post": :test,
         "coveralls.html": :test
       ],
-      deps: deps]
+      deps: deps
+    ]
   end
 
   # Configuration for the OTP application
   def application do
     [
-      applications: [:httpoison]
+      mod: {Stripe, []},
+      env: env(),
+      applications: apps(Mix.env)
     ]
   end
 
+  defp env() do
+    [
+      stripity_stripe: [
+        api_base_url: "https://api.stripe.com/v1/",
+        use_connection_pool: true,
+        pool_options: [
+          timeout: 5_000,
+          max_connections: 10
+        ]
+      ]
+    ]
+  end
+
+  defp apps(:test), do: [:bypass | apps()]
+  defp apps(_), do: apps()
+
+  defp apps(), do: [:hackney, :poison, :logger]
+
   defp deps do
     [
-      {:httpoison, ">= 0.0.0" },
-      {:poison, ">= 0.0.0", optional: true},
-      {:ex_doc, ">= 0.0.0", only: :dev},
-      {:earmark, ">= 0.0.0", only: :dev},
-      {:excoveralls, ">= 0.0.0", only: :test},
-      {:mock, ">= 0.0.0", only: :test},
-      {:inch_ex, ">= 0.0.0", only: [:dev, :test]}
+      {:bypass, "~> 0.5", only: :test},
+      {:earmark, "~> 1.0", only: :dev},
+      {:ex_doc, "~> 0.14", only: :dev},
+      {:excoveralls, "~> 0.5", only: :test},
+      {:hackney, "~> 1.6"},
+      {:inch_ex, "~> 0.5", only: [:dev, :test]},
+      {:poison, "~> 2.0 or ~> 3.0"}
     ]
   end
 
@@ -48,10 +70,8 @@ defmodule Stripe.Mixfile do
       maintainers: ["Dan Matthews", "Josh Smith"],
       licenses: ["New BSD"],
       links: %{
-        "GitHub" => "https://github.com/code-corps/stripity-stripe",
-        "Docs" => "https://hexdocs.pm/stripity_stripe"
+        "GitHub" => "https://github.com/code-corps/stripity-stripe"
       }
     ]
   end
-
 end
