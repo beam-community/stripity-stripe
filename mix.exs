08b9ea49cbc66798eab4e2e3515cb9a09c888cec
Josh Smith <joshdotsmith@gmail.com>
2016-11-22 17:21:03 -0800

    Bump version number

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Stripe.Mixfile do
         "coveralls.html": :test
       ],
       test_coverage: [tool: ExCoveralls],
-      version: "2.0.0-alpha.2"
+      version: "2.0.0-alpha.3"
     ]
   end
 
