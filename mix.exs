55dacd03b9ca9c751fa83ccf566f38532d56cdfb
David Antaramian <david@antaramian.com>
2016-11-27 23:50:09 -0500

    Allow for stripe_user params to be passed as part of connect URL

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Stripe.Mixfile do
         "coveralls.html": :test
       ],
       test_coverage: [tool: ExCoveralls],
-      version: "2.0.0-alpha.3"
+      version: "2.0.0-alpha.4"
     ]
   end
 
