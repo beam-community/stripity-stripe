9a716eb4a2c74fec2191d56991e427c0a9c21ccf
Nikola Begedin <begedinnikola@gmail.com>
2017-07-04 13:57:05 +0200

    Release alpha.9

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Stripe.Mixfile do
         "coveralls.html": :test
       ],
       test_coverage: [tool: ExCoveralls],
-      version: "2.0.0-alpha.8"
+      version: "2.0.0-alpha.9"
     ]
   end
 
