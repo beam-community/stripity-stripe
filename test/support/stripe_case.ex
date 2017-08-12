aa2d98431a41dada407b62517293823c38cbe4c3
Matt Jadczak <matt@mjdk.co.uk>
2017-08-12 20:19:48 +0100

    Make various tweaks to harness Test Stripe.Account

---
+++ test/support/stripe_case.ex
@@ -0,0 +1,35 @@
+defmodule Stripe.StripeCase do
+  @moduledoc """
+  This module defines the setup for tests requiring access to a mocked version of Stripe.
+  """
+
+  use ExUnit.CaseTemplate
+
+  def assert_stripe_requested(_method, _url) do
+    # TODO: use something akin to WebMock to check the API calls are correct
+    assert true
+  end
+
+  def stripe_base_url() do
+    Application.get_env(:stripity_stripe, :api_base_url)
+  end
+
+  using do
+    quote do
+      import Stripe.StripeCase, only: [assert_stripe_requested: 2, stripe_base_url: 0]
+    end
+  end
+
+  setup_all do
+    Stripe.StripeMock.reset()
+    Process.sleep(250)
+  end
+
+  setup tags do
+    on_exit fn ->
+      if tags[:reset_stripe_after] do
+        Stripe.StripeMock.reset()
+      end
+    end
+  end
+end
