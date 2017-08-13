6fd173deb756144b1e3513502f458e411126652a
Matt Jadczak <matt@mjdk.co.uk>
2017-08-13 20:47:50 +0100

    Add tests for Stripe.Refund

---
+++ test/stripe/objects/refund_test.exs
@@ -0,0 +1,36 @@
+defmodule Stripe.RefundTest do
+  use Stripe.StripeCase
+
+  test "is listable" do
+    assert {:ok, %Stripe.List{data: refunds}} = Stripe.Refund.list()
+    assert_stripe_requested :get, "/v1/refunds"
+    assert is_list(refunds)
+    assert %Stripe.Refund{} = hd(refunds)
+  end
+
+  test "is retrievable" do
+    assert {:ok, %Stripe.Refund{}} = Stripe.Refund.retrieve("re_123")
+    assert_stripe_requested :get, "/v1/refunds/re_123"
+  end
+
+  test "is creatable" do
+    assert {:ok, %Stripe.Refund{}} = Stripe.Refund.create(%{
+      charge: "ch_123"
+    })
+    assert_stripe_requested :post, "/v1/refunds"
+    reset_stripe()
+  end
+
+  test "is saveable" do
+    {:ok, refund} = Stripe.Refund.retrieve("re_123")
+    refund = put_in(refund.metadata["key"], "value")
+    assert {:ok, %Stripe.Refund{} = srefund} = Stripe.Refund.save(refund)
+    assert srefund.id == refund.id
+    assert_stripe_requested :post, "/v1/refunds/#{refund.id}"
+  end
+
+  test "is updateable" do
+    assert {:ok, refund} = Stripe.Refund.update("re_123", %{metadata: %{foo: "bar"}})
+    assert_stripe_requested :post, "/v1/refunds/#{refund.id}"
+  end
+end
