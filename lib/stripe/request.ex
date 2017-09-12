efac91ba1aa0acbbb3d60e86da4b36d8099ab5d6
Matt Jadczak <matt@mjdk.co.uk>
2017-09-12 22:06:00 +0100

    Stop validating required or valid fields on client-side

--- lib/stripe/request.ex
+++ lib/stripe/request.ex
@@ -11,16 +11,13 @@ defmodule Stripe.Request do
     endpoint: String.t | (map -> String.t),
     method: Stripe.API.method,
     params: %{optional(atom) => any},
-    valid_keys: nil | MapSet.t(atom),
-    required_keys: MapSet.t(atom),
     cast_to_id: MapSet.t(atom)
   }
 
-  @type error_code :: :valid_keys_failed | :required_keys_failed | :endpoint_fun_invalid_result
+  @type error_code ::  :endpoint_fun_invalid_result
     | :invalid_endpoint
 
-  defstruct opts: [], endpoint: nil, method: nil, params: %{}, valid_keys: nil,
-            required_keys: MapSet.new(), cast_to_id: MapSet.new
+  defstruct opts: [], endpoint: nil, method: nil, params: %{}, cast_to_id: MapSet.new
 
   @spec new_request(Stripe.options) :: t
   def new_request(opts \\ []) do
@@ -42,15 +39,9 @@ defmodule Stripe.Request do
     %{request | params: Map.merge(params, new_params)}
   end
 
-  @spec valid_keys(t, [atom]) :: t
-  def valid_keys(%Request{valid_keys: valid_keys} = request, new_valid_keys) do
-    valid_keys =
-      case valid_keys do
-        nil -> MapSet.new
-        valid_keys -> valid_keys
-      end
-
-    %{request | valid_keys: MapSet.union(valid_keys, MapSet.new(new_valid_keys))}
+  @spec put_param(t, atom, any) :: t
+  def put_param(%Request{params: params} = request, key, value) do
+    %{request | params: Map.put(params, key, value)}
   end
 
   @spec cast_to_id(t, [atom]) :: t
@@ -58,53 +49,26 @@ defmodule Stripe.Request do
     %{request | cast_to_id: MapSet.union(cast_to_id, MapSet.new(new_cast_to_id))}
   end
 
-  @spec required_keys(t, [atom]) :: t
-  def required_keys(%Request{required_keys: required_keys} = request, new_required_keys) do
-    %{request | required_keys: MapSet.union(required_keys, MapSet.new(new_required_keys))}
+  @spec cast_path_to_id(t, [atom]) :: t
+  def cast_path_to_id(%Request{cast_to_id: cast_to_id} = request, new_cast_to_id) do
+    %{request | cast_to_id: MapSet.put(cast_to_id, new_cast_to_id)}
   end
+
+  @spec get_id!(Stripe.id | struct) :: Stripe.id
+  def get_id!(id) when is_binary(id), do: id
+
+  def get_id!(%{id: id}) when is_binary(id), do: id
+
+  def get_id!(_), do: raise "You must provide an ID or a struct with an ID to this operation."
 
   @spec make_request(t) :: {:ok, struct} | {:error, Stripe.Error.t}
   def make_request(%Request{params: params, endpoint: endpoint, method: method, opts: opts} = request) do
     with\
-      :ok <- check_valid_keys(params, request.valid_keys),
-      :ok <- check_required_keys(params, request.required_keys),
       {:ok, params} <- do_cast_to_id(params, request.cast_to_id),
       {:ok, endpoint} <- consolidate_endpoint(endpoint, params),
       {:ok, result} <- API.request(params, method, endpoint, %{}, opts)
     do
       {:ok, Converter.convert_result(result)}
-    end
-  end
-
-  defp check_valid_keys(_, nil), do: :ok
-  defp check_valid_keys(params, valid_keys) do
-    invalid_keys =
-      params
-      |> Map.keys
-      |> MapSet.new
-      |> MapSet.difference(valid_keys)
-      |> MapSet.to_list
-
-    case invalid_keys do
-      [] -> :ok
-      invalid_keys ->
-        {:error, Stripe.Error.new(source: :internal, code: :valid_keys_failed,
-          message: "unknown param keys provided: #{inspect invalid_keys}")}
-    end
-  end
-
-  defp check_required_keys(params, required_keys) do
-    keyset =
-      params
-      |> Map.keys
-      |> MapSet.new
-
-    if MapSet.subset?(required_keys, keyset) do
-      :ok
-    else
-      missing_keys = MapSet.difference(required_keys, keyset) |> MapSet.to_list
-      {:error, Stripe.Error.new(source: :internal, code: :required_keys_failed,
-        message: "required param keys not provided: #{inspect missing_keys}")}
     end
   end
 
