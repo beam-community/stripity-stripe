df7d40764774ec1d37ea46c491a4251535066f34
Marcus Hammarberg <marcusoft.net@gmail.com>
2017-10-04 08:47:28 +0200

    Fixed heading in markdown doc (#258)
    * Fixed heading in markdown doc
    
    The configuration part of the first doc string was not rendered correctly. 
    
    There's a missing newline/empty line before the heading Configuration.
    
    Apparently, the heading (`#`) construct doesn't work in `@moduledoc`.
    
    Made a poor-mans-heading instead.

--- lib/stripe.ex
+++ lib/stripe.ex
@@ -3,7 +3,9 @@ defmodule Stripe do
     A HTTP client for Stripe.
     This module contains the Application that you can use to perform
     transactions on stripe API.
-    ### Configuring
+    
+    **Configuring**
+    
     By default the STRIPE_SECRET_KEY environment variable is used to find
     your API key for Stripe. You can also manually set your API key by
     configuring the :stripity_stripe application. You can see the default
