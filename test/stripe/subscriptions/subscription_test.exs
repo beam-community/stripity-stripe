2044034a7b5e5735b819eb8e6beb1c10507d50d2
Josh Smith <josh@codecorps.org>
2017-11-20 16:01:23 -0800

    Update Subscription tests

--- test/stripe/subscriptions/subscription_test.exs
+++ test/stripe/subscriptions/subscription_test.exs
@@ -1,39 +1,50 @@
 defmodule Stripe.SubscriptionTest do
   use Stripe.StripeCase, async: true
 
-    test "is listable" do
-      assert {:ok, %Stripe.List{data: subscriptions}} = Stripe.Subscription.list()
-      assert_stripe_requested :get, "/v1/subscriptions"
-      assert is_list(subscriptions)
-      assert %Stripe.Subscription{} = hd(subscriptions)
-    end
-
-    test "is retrievable" do
+  describe "retrieve/2" do
+    test "retrieves a subscription" do
       assert {:ok, %Stripe.Subscription{}} = Stripe.Subscription.retrieve("sub_123")
       assert_stripe_requested :get, "/v1/subscriptions/sub_123"
     end
+  end
 
-    test "is creatable" do
+  describe "create/2" do
+    test "creates a subscription" do
       assert {:ok, %Stripe.Subscription{}} = Stripe.Subscription.create(%{
         customer: "cus_123"
       })
       assert_stripe_requested :post, "/v1/subscriptions"
     end
+  end
 
-    test "is updateable" do
+  describe "update/2" do
+    test "updates a subscription" do
       assert {:ok, subscription} = Stripe.Subscription.update("sub_123", %{metadata: %{foo: "bar"}})
       assert_stripe_requested :post, "/v1/subscriptions/#{subscription.id}"
     end
 
-    test "is deletable" do
-      {:ok, subscription} = Stripe.Subscription.retrieve("sub_123")
-      assert {:ok, %Stripe.Subscription{}} = Stripe.Subscription.delete(subscription)
+  end
+  describe "delete/2" do
+    test "deletes a subscription" do
+      assert {:ok, %Stripe.Subscription{} = subscription} = Stripe.Subscription.delete("sub_123")
       assert_stripe_requested :delete, "/v1/subscriptions/#{subscription.id}"
     end
+  end
 
-    test "delete_discount/2 deletes a subscription's discount" do
+  describe "list/2" do
+    test "lists all subscriptions" do
+      assert {:ok, %Stripe.List{data: subscriptions}} = Stripe.Subscription.list()
+      assert_stripe_requested :get, "/v1/subscriptions"
+      assert is_list(subscriptions)
+      assert %Stripe.Subscription{} = hd(subscriptions)
+    end
+  end
+
+  describe "delete_discount/2" do
+    test "deletes a subscription's discount" do
       {:ok, subscription} = Stripe.Subscription.retrieve("sub_123")
-      assert {:ok, _} = Stripe.Subscription.delete_discount(subscription)
+      assert {:ok, _} = Stripe.Subscription.delete_discount("sub_123")
       assert_stripe_requested :delete, "/v1/subscriptions/#{subscription.id}/discount"
     end
+  end
 end
