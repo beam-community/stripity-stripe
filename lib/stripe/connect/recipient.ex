96992022a54c45d18624a8bd7ae1bc71bb1998f5
Josh Smith <josh@codecorps.org>
2017-11-21 16:07:17 -0800

    Format Recipient

--- lib/stripe/connect/recipient.ex
+++ lib/stripe/connect/recipient.ex
@@ -8,39 +8,39 @@ defmodule Stripe.Recipient do
   use Stripe.Entity
 
   @type t :: %__MODULE__{
-               id: Stripe.id,
-               object: String.t,
-               active_account: nil | %{
-                 id: Stripe.id,
-                 object: String.t,
-                 account: Stripe.id,
-                 account_holder_name: String.t,
-                 account_holder_type: String.t,
-                 bank_name: String.t,
-                 country: String.t,
-                 currency: String.t,
-                 customer: Stripe.id,
-                 default_for_currency: boolean,
-                 fingerprint: String.t,
-                 last4: String.t,
-                 metadata: %{
-                   optional(String.t) => String.t
-                 },
-                 routing_number: String.t,
-                 status: String.t
-               },
-               cards: Stripe.List.of(Stripe.Card.t),
-               created: Stripe.timestamp,
-               default_card: Stripe.id | Stripe.Card.t,
-               description: String.t,
-               email: String.t,
-               livemode: boolean,
-               metadata: Stripe.Types.metadata,
-               migrated_to: Stripe.id | Stripe.Account.t,
-               name: String.t,
-               rolled_back_from: String.t,
-               type: String.t
-             }
+          id: Stripe.id(),
+          object: String.t(),
+          active_account:
+            %{
+              id: Stripe.id(),
+              object: String.t(),
+              account: Stripe.id(),
+              account_holder_name: String.t(),
+              account_holder_type: String.t(),
+              bank_name: String.t(),
+              country: String.t(),
+              currency: String.t(),
+              customer: Stripe.id(),
+              default_for_currency: boolean,
+              fingerprint: String.t(),
+              last4: String.t(),
+              metadata: Stripe.Types.metadata(),
+              routing_number: String.t(),
+              status: String.t()
+            }
+            | nil,
+          cards: Stripe.List.of(Stripe.Card.t()),
+          created: Stripe.timestamp(),
+          default_card: Stripe.id() | Stripe.Card.t(),
+          description: String.t(),
+          email: String.t(),
+          livemode: boolean,
+          metadata: Stripe.Types.metadata(),
+          migrated_to: Stripe.id() | Stripe.Account.t(),
+          name: String.t(),
+          rolled_back_from: String.t(),
+          type: String.t()
+        }
 
   defstruct [
     :id,
