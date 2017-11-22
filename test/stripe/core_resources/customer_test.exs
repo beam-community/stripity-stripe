c4d820760d358551a4f636c3a331f13d321c581c
Josh Smith <josh@codecorps.org>
2017-11-21 16:14:50 -0800

    Format Customer

--- test/stripe/core_resources/customer_test.exs
+++ test/stripe/core_resources/customer_test.exs
@@ -3,28 +3,29 @@ defmodule Stripe.CustomerTest do
 
   test "is creatable" do
     assert {:ok, %Stripe.Customer{}} = Stripe.Customer.create(%{})
-    assert_stripe_requested :post, "/v1/customers"
+    assert_stripe_requested(:post, "/v1/customers")
   end
 
   test "is retrievable" do
     assert {:ok, %Stripe.Customer{}} = Stripe.Customer.retrieve("cus_123")
-    assert_stripe_requested :get, "/v1/customers/cus_123"
+    assert_stripe_requested(:get, "/v1/customers/cus_123")
   end
 
   test "is updateable" do
-    assert {:ok, %Stripe.Customer{}} = Stripe.Customer.update("cus_123", %{metadata: %{key: "value"}})
-    assert_stripe_requested :post, "/v1/customers/cus_123"
+    params = %{metadata: %{key: "value"}}
+    assert {:ok, %Stripe.Customer{}} = Stripe.Customer.update("cus_123", params)
+    assert_stripe_requested(:post, "/v1/customers/cus_123")
   end
 
   test "is deleteable" do
     {:ok, customer} = Stripe.Customer.retrieve("cus_123")
     assert {:ok, %Stripe.Customer{}} = Stripe.Customer.delete(customer)
-    assert_stripe_requested :delete, "/v1/customers/#{customer.id}"
+    assert_stripe_requested(:delete, "/v1/customers/#{customer.id}")
   end
 
   test "is listable" do
     assert {:ok, %Stripe.List{data: customers}} = Stripe.Customer.list()
-    assert_stripe_requested :get, "/v1/customers"
+    assert_stripe_requested(:get, "/v1/customers")
     assert is_list(customers)
     assert %Stripe.Customer{} = hd(customers)
   end
