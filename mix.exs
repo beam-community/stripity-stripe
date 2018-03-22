3c72b19e08596c53fdcd1992fb6dea318fc0295e
Nikola Begedin <begedinnikola@gmail.com>
2018-03-22 09:48:49 +0100

    Update version to 2.0.0-alpha.11

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Stripe.Mixfile do
         "coveralls.html": :test
       ],
       test_coverage: [tool: ExCoveralls],
-      version: "2.0.0-alpha.10"
+      version: "2.0.0-alpha.11"
     ]
   end
 
