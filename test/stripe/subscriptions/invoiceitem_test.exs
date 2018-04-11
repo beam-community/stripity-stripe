464687ef1bfad6512ae1998d45187832e5ffbf8f
Scott Newcomer <snewcomer@users.noreply.github.com>
2018-04-11 07:48:23 -0700

    Update invoice types and params (#346)
    * update invoice types and params
    
    * remove card params check
    
    * update invoiceitem with types, params and endpoints
    
    * invoice item test
    
    * restart test
    
    * add product test

---
+++ test/stripe/subscriptions/invoiceitem_test.exs
@@ -0,0 +1,34 @@
+defmodule Stripe.InvoiceitemTest do
+  use Stripe.StripeCase, async: true
+
+  describe "create/2" do
+    test "creates an invoice" do
+      assert {:ok, %Stripe.Invoiceitem{}} = Stripe.Invoiceitem.create(%{customer: "cus_123", currency: "usd"})
+      assert_stripe_requested(:post, "/v1/invoiceitems")
+    end
+  end
+
+  describe "retrieve/2" do
+    test "retrieves an invoice" do
+      assert {:ok, %Stripe.Invoiceitem{}} = Stripe.Invoiceitem.retrieve("in_1234")
+      assert_stripe_requested(:get, "/v1/invoiceitems/in_1234")
+    end
+  end
+
+  describe "update/2" do
+    test "updates an invoice" do
+      params = %{metadata: %{key: "value"}}
+      assert {:ok, %Stripe.Invoiceitem{}} = Stripe.Invoiceitem.update("in_1234", params)
+      assert_stripe_requested(:post, "/v1/invoiceitems/in_1234")
+    end
+  end
+
+  describe "list/2" do
+    test "lists all invoiceitems" do
+      assert {:ok, %Stripe.List{data: invoiceitems}} = Stripe.Invoiceitem.list()
+      assert_stripe_requested(:get, "/v1/invoiceitems")
+      assert is_list(invoiceitems)
+      assert %Stripe.Invoiceitem{} = hd(invoiceitems)
+    end
+  end
+end
