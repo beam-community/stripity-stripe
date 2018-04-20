3c92ce0fceb235f151b92112d4b41d9ad4e3f96d
snewcomer <snewcomer24@gmail.com>
2018-04-20 12:49:41 -0700

    1.4 elixir

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Stripe.Mixfile do
       deps: deps(),
       description: description(),
       dialyzer: [plt_add_apps: [:mix]],
-      elixir: "~> 1.5",
+      elixir: "~> 1.4",
       package: package(),
       elixirc_paths: elixirc_paths(Mix.env()),
       preferred_cli_env: [
