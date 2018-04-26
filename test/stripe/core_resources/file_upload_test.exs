9bf9e1cc162e0818921407fce31569077cc384a2
snewcomer <snewcomer24@gmail.com>
2018-04-26 16:00:13 -0700

    bring back file upload tests

--- test/stripe/core_resources/file_upload_test.exs
+++ test/stripe/core_resources/file_upload_test.exs
@@ -2,7 +2,6 @@ defmodule Stripe.FileUploadTest do
   use Stripe.StripeCase, async: true
 
   describe "create/2" do
-    @tag :skip
     test "creates a file" do
       assert {:ok, %Stripe.FileUpload{}} = Stripe.FileUpload.create(%{
         file: "@/path/to/a/file.jpg",
@@ -13,7 +12,6 @@ defmodule Stripe.FileUploadTest do
   end
 
   describe "retrieve/2" do
-    @tag :skip
     test "retrieves an file" do
       assert {:ok, %Stripe.FileUpload{}} = Stripe.FileUpload.retrieve("file_123")
       assert_stripe_requested :get, "/v1/files/file_123"
@@ -21,7 +19,6 @@ defmodule Stripe.FileUploadTest do
   end
 
   describe "list/2" do
-    @tag :skip
     test "lists all files" do
       assert {:ok, %Stripe.List{data: [%Stripe.FileUpload{}]}} = Stripe.FileUpload.list()
       assert_stripe_requested :get, "/v1/files"
