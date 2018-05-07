0054fee0474dff3196522cfd0aa1de9fb7bf8b28
snewcomer <snewcomer24@gmail.com>
2018-05-07 08:23:56 -0500

    cleanup test

--- test/stripe/relay/product_test.exs
+++ test/stripe/relay/product_test.exs
@@ -2,21 +2,21 @@ defmodule Stripe.Relay.ProductTest do
   use Stripe.StripeCase, async: true
 
   describe "create/2" do
-    test "creates an invoice" do
+    test "creates an product" do
       assert {:ok, %Stripe.Product{}} = Stripe.Product.create(%{name: "Plus", type: "service"})
       assert_stripe_requested(:post, "/v1/products")
     end
   end
 
   describe "retrieve/2" do
-    test "retrieves an invoice" do
+    test "retrieves an product" do
       assert {:ok, %Stripe.Product{}} = Stripe.Product.retrieve("Plus")
       assert_stripe_requested(:get, "/v1/products/Plus")
     end
   end
 
   describe "update/2" do
-    test "updates an invoice" do
+    test "updates an product" do
       params = %{metadata: %{key: "value"}}
       assert {:ok, %Stripe.Product{}} = Stripe.Product.update("Plus", params)
       assert_stripe_requested(:post, "/v1/products/Plus")
@@ -26,6 +26,14 @@ defmodule Stripe.Relay.ProductTest do
   describe "list/2" do
     test "lists all products" do
       assert {:ok, %Stripe.List{data: products}} = Stripe.Product.list()
+      assert_stripe_requested(:get, "/v1/products")
+      assert is_list(products)
+      assert %Stripe.Product{} = hd(products)
+    end
+
+    test "lists all products with params" do
+      params = %{active: false}
+      assert {:ok, %Stripe.List{data: products}} = Stripe.Product.list(params)
       assert_stripe_requested(:get, "/v1/products")
       assert is_list(products)
       assert %Stripe.Product{} = hd(products)
