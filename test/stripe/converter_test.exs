ae55f11f4b1051a11e9831d880cae275b021ee5b
snewcomer <snewcomer24@gmail.com>
2018-08-03 06:40:14 -0700

    fix test

--- test/stripe/converter_test.exs
+++ test/stripe/converter_test.exs
@@ -205,6 +205,7 @@ defmodule Stripe.ConverterTest do
         fingerprint: "1JWtPxqbdX5Gamtc",
         last4: "6789",
         metadata: %{},
+        name: "Student Discount",
         routing_number: "110000000",
         status: "new"
       },
