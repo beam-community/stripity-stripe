ec24afbd49fc1851fc8f74ee9a36f764e1ac44ca
Michael Ries <michael@riesd.com>
2018-11-09 15:45:18 -0700

    load dialyxir in the test enviroment

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule Stripe.Mixfile do
   defp deps do
     [
       {:bypass, "~> 0.8.1", only: :test},
-      {:dialyxir, "1.0.0-rc.4", only: [:dev], runtime: false},
+      {:dialyxir, "1.0.0-rc.4", only: [:dev, :test], runtime: false},
       {:earmark, "~> 1.2.5", only: :dev},
       {:ex_doc, "~> 0.18.3", only: :dev},
       {:excoveralls, "~> 0.8.1", only: :test},
