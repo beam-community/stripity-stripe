c4a9764dc8086105ed015546ad7d155412b7eeeb
Jason Cartwright <jcartwright@Jasons-MBP.fios-router.home>
2019-05-08 13:36:23 -0500

    Add support for Stripe.Issuing
    - Issuing.Authorization
    - Issuing.Card
    - Issuing.CardDetails
    - Issuing.Cardholder
    - Issuing.Dispute
    - Issuing.Transaction

--- lib/stripe/types.ex
+++ lib/stripe/types.ex
@@ -12,6 +12,30 @@ defmodule Stripe.Types do
           state: String.t() | nil
         }
 
+  @type authorization_controls :: %{
+          allowed_categories: list() | nil,
+          blocked_categories: list() | nil,
+          spending_limits: list(Stripe.Types.spending_limits()) | nil,
+          currency: String.t() | nil,
+          max_amount: non_neg_integer | nil,
+          max_approvals: non_neg_integer | nil
+        }
+
+  @type billing :: %{
+          address: Stripe.Types.address(),
+          name: String.t()
+        }
+
+  @type evidence :: %{
+          fraudulent: Stripe.Types.evidence_detail() | nil,
+          other: Stripe.Types.evidence_detail() | nil
+        }
+
+  @type evidence_detail :: %{
+          dispute_explanation: String.t(),
+          uncategorized_file: String.t()
+        }
+
   @type fee :: %{
           amount: integer,
           application: String.t() | nil,
@@ -20,16 +44,45 @@ defmodule Stripe.Types do
           type: String.t()
         }
 
+  @type merchant_data :: %{
+          category: String.t(),
+          city: String.t(),
+          country: String.t(),
+          name: String.t(),
+          network_id: String.t(),
+          postal_code: String.t(),
+          state: String.t()
+        }
+
   @type metadata :: %{
           optional(String.t()) => String.t()
+        }
+
+  @type request_history :: %{
+          approved: boolean,
+          authorized_amount: integer,
+          authorized_currency: String.t(),
+          created: Stripe.timestamp(),
+          held_amount: integer,
+          held_currency: String.t(),
+          reason: String.t()
         }
 
   @type shipping :: %{
           address: Stripe.Types.address(),
           carrier: String.t() | nil,
+          eta: Stripe.timestamp() | nil,
           name: String.t(),
           phone: String.t() | nil,
-          tracking_number: String.t() | nil
+          status: String.t() | nil,
+          tracking_number: String.t() | nil,
+          tracking_url: String.t() | nil
+        }
+
+  @type spending_limits :: %{
+          amount: non_neg_integer,
+          categories: list(),
+          interval: String.t()
         }
 
   @type tax_info :: %{
@@ -47,5 +100,11 @@ defmodule Stripe.Types do
           interval: String.t(),
           monthly_anchor: non_neg_integer | nil,
           weekly_anchor: String.t() | nil
+        }
+
+  @type verification_data :: %{
+          address_line1_check: String.t(),
+          address_zip_check: String.t(),
+          cvc_check: String.t()
         }
 end
