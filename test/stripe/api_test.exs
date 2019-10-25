87dedf6584f9ba7ec5222ab86e3eeca4f751e10d
snewcomer <snewcomer24@gmail.com>
2019-10-25 10:13:41 -0700

    fix defeault api version

--- test/stripe/api_test.exs
+++ test/stripe/api_test.exs
@@ -85,7 +85,7 @@ defmodule Stripe.APITest do
 
   test "gets default api version" do
     Stripe.API.request(%{}, :get, "products", %{}, [])
-    assert_stripe_requested(:get, "/v1/products", headers: {"Stripe-Version", "2019-05-16"})
+    assert_stripe_requested(:get, "/v1/products", headers: {"Stripe-Version", "2019-10-17"})
   end
 
   test "can set custom api version" do
