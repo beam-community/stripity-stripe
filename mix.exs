4e94c1e0deb0b90ec6272c380e254784a2b04e98
Scott Newcomer <snewcomer@users.noreply.github.com>
2020-06-29 07:01:46 -0500

    VERSION BUMP 2.9.0 (#614)
    * VERSION BUMP 2.9.0
    
    * CHANGELOG

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Stripe.Mixfile do
         "coveralls.html": :test
       ],
       test_coverage: [tool: ExCoveralls],
-      version: "2.8.0",
+      version: "2.9.0",
       source_url: "https://github.com/code-corps/stripity_stripe/",
       docs: [
         main: "readme",
