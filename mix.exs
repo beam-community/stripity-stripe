4dcd8492f3d4f057344aa56b28af325135222830
Scott Newcomer <snewcomer24@gmail.com>
2021-06-20 22:24:06 -0500

    [CI]: actions instead of travisci

--- mix.exs
+++ mix.exs
@@ -65,7 +65,7 @@ defmodule Stripe.Mixfile do
     [
       {:dialyxir, "1.0.0-rc.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.23.0", only: :dev},
-      {:excoveralls, "~> 0.11.1", only: :test},
+      {:excoveralls, "~> 0.14.1", only: :test},
       {:hackney, "~> 1.15"},
       {:inch_ex, "~> 2.0", only: [:dev, :test]},
       {:mox, "~> 0.4", only: :test},
