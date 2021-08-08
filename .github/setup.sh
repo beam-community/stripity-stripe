17c0d1acc12d046eaab202cd13a133ec1a7c7539
Scott Newcomer <snewcomer24@gmail.com>
2021-08-08 09:01:31 -0500

    use env var

--- .github/setup.sh
+++ .github/setup.sh
@@ -1,5 +1,5 @@
-if [ ! -d "stripe-mock/stripe-mock_0.95.0" ]; then
-    mkdir -p stripe-mock/stripe-mock_0.95.0/
-    curl -L "https://github.com/stripe/stripe-mock/releases/download/v0.95.0/stripe-mock_0.95.0_linux_amd64.tar.gz" -o "stripe-mock/stripe-mock_0.95.0_linux_amd64.tar.gz"
-    tar -zxf "stripe-mock/stripe-mock_0.95.0_linux_amd64.tar.gz" -C "stripe-mock/stripe-mock_0.95.0/"
+if [ ! -d "stripe-mock/stripe-mock_${STRIPE_MOCK_VERSION}" ]; then
+    mkdir -p stripe-mock/stripe-mock_${STRIPE_MOCK_VERSION}/
+    curl -L "https://github.com/stripe/stripe-mock/releases/download/v${STRIPE_MOCK_VERSION}/stripe-mock_${STRIPE_MOCK_VERSION}_linux_amd64.tar.gz" -o "stripe-mock/stripe-mock_${STRIPE_MOCK_VERSION}_linux_amd64.tar.gz"
+    tar -zxf "stripe-mock/stripe-mock_${STRIPE_MOCK_VERSION}_linux_amd64.tar.gz" -C "stripe-mock/stripe-mock_${STRIPE_MOCK_VERSION}/"
 fi
