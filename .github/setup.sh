500dadef990a72c2cf839760928e9eca8e2d1a95
Scott Newcomer <snewcomer24@gmail.com>
2021-08-08 08:08:09 -0500

    debug

--- .github/setup.sh
+++ .github/setup.sh
@@ -1,5 +1,5 @@
-if [ ! -d "stripe-mock/stripe-mock_${0.95.0}" ]; then
-    mkdir -p stripe-mock/stripe-mock_${0.95.0}/
-    curl -L "https://github.com/stripe/stripe-mock/releases/download/v${0.95.0}/stripe-mock_${0.95.0}_linux_amd64.tar.gz" -o "stripe-mock/stripe-mock_${0.95.0}_linux_amd64.tar.gz"
-    tar -zxf "stripe-mock/stripe-mock_${0.95.0}_linux_amd64.tar.gz" -C "stripe-mock/stripe-mock_${STRIPE_MOCK_VERSION}/"
+if [ ! -d "stripe-mock/stripe-mock_0.95.0" ]; then
+    mkdir -p stripe-mock/stripe-mock_0.95.0/
+    curl -L "https://github.com/stripe/stripe-mock/releases/download/v0.95.0/stripe-mock_0.95.0_linux_amd64.tar.gz" -o "stripe-mock/stripe-mock_0.95.0_linux_amd64.tar.gz"
+    tar -zxf "stripe-mock/stripe-mock_0.95.0_linux_amd64.tar.gz" -C "stripe-mock/stripe-mock_0.95.0/"
 fi
