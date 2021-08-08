a2d5ec7d284b770489bbb795f57c9b22fde92bad
Scott Newcomer <snewcomer24@gmail.com>
2021-08-08 10:30:00 -0500

    delete not needed

--- .github/setup.sh
+++
@@ -1,5 +0,0 @@
-if [ ! -d "stripe-mock/stripe-mock_${STRIPE_MOCK_VERSION}" ]; then
-    mkdir -p stripe-mock/stripe-mock_${STRIPE_MOCK_VERSION}/
-    curl -L "https://github.com/stripe/stripe-mock/releases/download/v${STRIPE_MOCK_VERSION}/stripe-mock_${STRIPE_MOCK_VERSION}_linux_amd64.tar.gz" -o "stripe-mock/stripe-mock_${STRIPE_MOCK_VERSION}_linux_amd64.tar.gz"
-    tar -zxf "stripe-mock/stripe-mock_${STRIPE_MOCK_VERSION}_linux_amd64.tar.gz" -C "stripe-mock/stripe-mock_${STRIPE_MOCK_VERSION}/"
-fi
