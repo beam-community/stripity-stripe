87056a2e0bff4d525ecc44eb2a9182f33c3d069a
Scott Newcomer <snewcomer24@gmail.com>
2021-08-15 22:12:06 -0500

    try this

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -10,8 +10,8 @@ unless System.get_env("SKIP_STRIPE_MOCK_RUN") do
   {:ok, _pid} = Stripe.StripeMock.start_link(port: 12111, global: true)
 end
 
-api_base_url = System.get_env("STRIPE_API_BASE_URL") || "http://localhost:12123/v1/"
-api_upload_url = System.get_env("STRIPE_API_UPLOAD_URL") || "http://localhost:12123/v1/"
+api_base_url = System.get_env("STRIPE_API_BASE_URL") || "http://localhost:12111/v1/"
+api_upload_url = System.get_env("STRIPE_API_UPLOAD_URL") || "http://localhost:12112/v1/"
 
 Application.put_env(:stripity_stripe, :api_base_url, api_base_url)
 Application.put_env(:stripity_stripe, :api_upload_url, api_upload_url)
@@ -41,3 +41,6 @@ defmodule Helper do
   end
 end
 
+unless System.get_env("SKIP_STRIPE_MOCK_RUN") do
+  Helper.wait_until_stripe_mock_launch()
+end
