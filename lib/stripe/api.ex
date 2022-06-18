0e91c2f88ffe59c8dc4133f8be3dd98e7b97a566
Scott Newcomer <snewcomer24@gmail.com>
2022-06-17 21:40:48 -0500

    bugfix 429 retry

--- lib/stripe/api.ex
+++ lib/stripe/api.ex
@@ -428,7 +428,7 @@ defmodule Stripe.API do
   # 409 conflict
   defp retry_response?({:error, 409, _headers, _body}), do: true
   # https://github.com/beam-community/stripity_stripe/issues/686
-  defp retry_response?({:errro, 429, _headers, _body}), do: true
+  defp retry_response?({:error, 429, _headers, _body}), do: true
   # Destination refused the connection, the connection was reset, or a
   # variety of other connection failures. This could occur from a single
   # saturated server, so retry in case it's intermittent.
